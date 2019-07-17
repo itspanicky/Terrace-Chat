8dde7b18ed2739009884f693ba51a0346359f674
github username <jonathansychan@gmail.com>
2019-07-17 15:25:23 -0400

    Sets up client folder and some scripts

--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ app.get("/", (req, res) => {
     res.send("Hello World!");   // test message
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
