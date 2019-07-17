f803f6f5b10ccb94fb2e6e95bb7cbb0d978cbb42
itspanicky <nicky.li92@gmail.com>
2019-07-17 17:00:33 -0400

    Create splash page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,32 @@
 import React from 'react'; 
+import '../stylesheets/Header2.css'
 
 const Header = () => {
 
+    // return (
+    //     <div className="header-container">
+    //         <div className="logo-name">TERRACE</div>
+    //         <div className="logo-name">CHAT</div>
+    //         <div className="logo-line">
+    //             インターネット上で
+    //         </div>
+    //         <div>
+                
+    //         </div>
+    //     </div>
+    // )
+
     return (
-        <div>
-            <h1>TERRACE CHAT</h1>
+        <div className="header-container">
+            <div className="logo-name">TERRACE CHAT</div>
+            <div className="logo-side">
+                <div className="logo-motto">ON THE</div>
+                <div className="logo-motto">INTERNET</div>
+                <div className="logo-line">
+                    ウ ェ ブ 上 で
+                </div>
+            </div>
+            
         </div>
     )
 }
