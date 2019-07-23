5785669e9d573cffed9157b16c9035d539d8f951
itspanicky <nicky.li92@gmail.com>
2019-07-23 10:52:52 -0400

    Add redux components to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import { AuthRoute, ProtectedRoute } from './util/route_util';
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
