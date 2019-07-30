a95afc7f197d7e50a25a35b6797e13c62914a066
itspanicky <nicky.li92@gmail.com>
2019-07-30 16:42:49 -0400

    Remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Home from './components/home/home_container';
 import './App.css';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from './util/route_util';
 import { Switch } from 'react-router-dom';
 import LoginFormContainer from './components/session/login_form_container';
