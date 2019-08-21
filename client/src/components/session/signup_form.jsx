3d3039cd8cd318d07f0910fddf960e3ba51ad4c6
itspanicky <nicky.li92@gmail.com>
2019-08-21 15:58:03 -0400

    Style signup form

--- client/src/components/session/signup_form.jsx
+++ client/src/components/session/signup_form.jsx
@@ -57,74 +57,124 @@ class SignupForm extends React.Component {
             </ul>
         )
     }
+
+    animateForm() {
+        const arrows = document.querySelectorAll(".fa-arrow-down")
+        arrows.forEach(arrow => {
+            arrow.addEventListener("click", () => {
+                const input = arrow.previousElementSibling
+                const parent = arrow.parentElement
+                console.log(input)
+            })
+        })
+    }
     
     render() {
         return (
-            <>
-                <div className="header-container header-animate">
-                    <div className="logo-name">TERRACE CHAT</div>
-                    <Link to="/" className="logo-side">
-                        <div className="logo-motto">ON THE</div>
-                        <div className="logo-motto">INTERNET</div>
-                        <div className="logo-line">
-                            ウ ェ ブ 上 で
-                        </div>
-                    </Link>
+          <>
+            <div className="header-container header-animate">
+              <div className="logo-name">TERRACE CHAT</div>
+              <Link to="/" className="logo-side">
+                <div className="logo-motto">ON THE</div>
+                <div className="logo-motto">INTERNET</div>
+                <div className="logo-line">ウ ェ ブ 上 で</div>
+              </Link>
+            </div>
+
+            <div className="session-form-container">
+              <form onSubmit={this.handleSubmit}>
+                <div className="session-form">
+                  <br />
+
+                  <div className="field-email">
+                    <input
+                      type="text"
+                      value={this.state.email}
+                      onChange={this.update("email")}
+                      placeholder="Email"
+                      autoComplete="email"
+                    />
+                    <i className="fas fa-arrow-down" />
+                  </div>
+
+                  <br />
+
+                  <div className="field-name inactive">
+                    <input
+                      type="text"
+                      value={this.state.name}
+                      onChange={this.update("name")}
+                      placeholder="name"
+                      autoComplete="username"
+                    />
+                    <i className="fas fa-arrow-down" />
+                  </div>
+
+                  <br />
+                  <div className="field-dob inactive">
+                    <input
+                      type="date"
+                      value={this.state.dob}
+                      onChange={this.update("dob")}
+                      placeholder="Date of birth"
+                    />
+                    <i className="fas fa-arrow-down" />
+                  </div>
+
+                  <br />
+
+                  <div className="field-gender inactive">
+                    <select
+                      value={this.state.gender}
+                      onChange={this.update("gender")}
+                    >
+                      <option value="0">Select Gender</option>
+                      <option value="Male">Male</option>
+                      <option value="Female">Female</option>
+                    </select>
+                  </div>
+
+                  <br />
+
+                  <div className="field-password inactive">
+                    <input
+                      type="password"
+                      value={this.state.password}
+                      onChange={this.update("password")}
+                      placeholder="Password"
+                      autoComplete="new-password"
+                    />
+                    <i className="fas fa-arrow-down" />
+                  </div>
+
+                  <br />
+
+                  <div className="field-password2 inactive">
+                    <input
+                      type="password"
+                      value={this.state.password2}
+                      onChange={this.update("password2")}
+                      placeholder="Confirm Password"
+                      autoComplete="new-password"
+                    />
+                    <i className="fas fa-arrow-down" />
+                  </div>
+
+                  <br />
+
+                  <div className="field-submit inactive">
+                    <input type="submit" value="Submit" />
+                  </div>
+                  {this.renderErrors()}
                 </div>
-                <div className="session-form-container">
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="session-form">
-                            <br />
-                            <input type="text"
-                                value={this.state.email}
-                                onChange={this.update('email')}
-                                placeholder="Email"
-                                autoComplete="email"
-                            />
-                            <br />
-                            <input type="text"
-                                value={this.state.name}
-                                onChange={this.update('name')}
-                                placeholder="name"
-                                autoComplete="username"
-                            />
-                            <br />
-                            <input type="date"
-                                value={this.state.dob}
-                                onChange={this.update('dob')}
-                                placeholder="Date of birth"
-                            />
-                            <br />
-                            <select
-                                value={this.state.gender}
-                                onChange={this.update('gender')}>
-                                <option value="0">Select Gender</option>
-                                <option value="Male">Male</option>
-                                <option value="Female">Female</option>
-                            </select>
-                            <br />
-                            <input type="password"
-                                value={this.state.password}
-                                onChange={this.update('password')}
-                                placeholder="Password"
-                                autoComplete="new-password"
-                            />
-                            <br />
-                            <input type="password"
-                                value={this.state.password2}
-                                onChange={this.update('password2')}
-                                placeholder="Confirm Password"
-                                autoComplete="new-password"
-                            />
-                            <br />
-                            <input type="submit" value="Submit" />
-                            {this.renderErrors()}
-                        </div>
-                    </form>
-                </div>
-                <Link className="link_to" to='/login'>Log in</Link>
-            </>
-        )
+              </form>
+            </div>
+
+            <Link className="link_to" to="/login">
+              Log in
+            </Link>
+          </>
+        );
     }
 }
 
