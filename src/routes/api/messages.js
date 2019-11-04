9090ca8635bbf7b3859363ae091954cc44f59b29
github username <jonathansychan@gmail.com>
2019-11-04 15:31:30 -0500

    Adds documentation and adds user updates on private room creation.

--- src/routes/api/messages.js
+++ src/routes/api/messages.js
@@ -5,24 +5,37 @@ const Room = require("../../models/Room");
 const Message = require("../../models/Message");
 
 // Create Message '/api/messages/create'
+// EXAMPLE BODY POST REQUEST
+// {
+// 	"room": "5dbf2435c4c2886ccb15b95d",
+// 	"text": "Nickys wak"
+// }
 router.post("/create", auth, async (req, res) => {
     const user = req.user;
     const roomId = req.body.room
-    // CONCERNS
-    // Validate that you belong in this room.
-    //  [code here]
-    // Needs to update Room model messages with MessageID
-
+    
     req.body.user = user._id
-  try {
-      const message = new Message(req.body);
-      await message.save();
-      const room = await Room.findOne({ _id: roomId})
-      await room.updateOne({ messages: [...room.messages, message._id]})
-      await room.save()
+    try {
+          // Validate that room exists.
+          if (!roomId) {
+            throw new Error("Need room Id.");
+          }
+          // Validate that you belong in room.
+          if (
+            !user.privaterooms.filter(room => room._id === roomId) &&
+            user.livingroom._id !== roomId
+          ) {
+            throw new Error("User does not belong in this room.");
+          }
+          const message = new Message(req.body);
+          await message.save();
+          // Needs to update Room model messages with MessageID
+          const room = await Room.findOne({ _id: roomId });
+          await room.updateOne({ messages: [...room.messages, message._id] });
+          await room.save();
 
-    res.status(201).send({ message });
-  } catch (e) {
+          res.status(201).send({ message });
+        } catch (e) {
     res.status(400).send(e);
   }
 });
--- src/routes/api/rooms.js
+++ src/routes/api/rooms.js
@@ -2,50 +2,64 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const Room = require("../../models/Room");
+const User = require("../../models/User");
 const validateRoom = require("../../validation/room");
 
 // Create Room '/api/rooms/create'
 // Mainly for private chats
+// EXAMPLE BODY POST REQUEST
+// {
+// 	"usersInRoom": [
+// 		{"_id": "5dbf202f651dcd6b88c666d0"}
+// 		]
+// }
 router.post('/create', auth, async (req, res) => {
   // CONCERNS
-  // Need to update Users' privaterooms  with this ObjectId
-
-  const user = req.user 
-  // Assumes frontend Post doesn't have own user id.
-  req.body.usersInRoom.push({_id: user.id})
-  const { errors, isValid } = validateRoom(req.body);
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  
   // Look for 3 males and 3 females in database and form livingroom.
   try {
-        const room = new Room(req.body);
-        await room.save();
-        res.status(201).send({ room });
-      } catch (e) {
+    const user = req.user 
+    // Assumes frontend Post doesn't have own user id.
+    const { errors, isValid } = validateRoom(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    req.body.usersInRoom.push({_id: user.id})
+    const room = new Room(req.body);
+    await room.save();
+    // Need to update Users' privaterooms  with this ObjectId
+    req.body.usersInRoom.forEach( async (user) => {
+      let currUser = await User.findOne({_id: user._id})
+      await currUser.updateOne({privaterooms: [...currUser.privaterooms, room._id]})
+    })
+    res.status(201).send({ room });
+    } catch (e) {
     res.status(400).send(e)
   }
 })
 
-// GET all messages in Room
+// GET all messages and userIds in Room
 // Example Query {{url}}/api/rooms/messages?roomId=5dbf2435c4c2886ccb15b95d
 router.get('/messages', auth, async (req, res) => {
   const { roomId, start, end } = req.query
+  const limitAmt = end - start
   try {
-    const room = await Room.findOne({ _id: roomId}).populate({
-      path: 'Message',
-      options: {
-        limit: 15,
-        skip: start
-      }
-    })
+    // if there is a specified params
     if (start && end) {
+      const room = await Room.findOne({ _id: roomId}).populate({
+        path: 'Message',
+        options: {
+          limit: limitAmt,
+          skip: end
+        }
+      })
       // assume that array in database is in order based on recency. 
-      res.status(201).send({ messages: room.messages })
+      res.status(201).send({ room })
     } else {
+      // sends all messages if params not specified.
+      const room = await Room.findOne({ _id: roomId})
       room.populate({ path: 'Message' })
-      res.status(201).send({ messages: room.messages })
+      res.status(201).send({ room })
     }
   } catch (e) {
     res.status(400).send(e)
